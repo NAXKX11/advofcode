3d196a769ebe54537742d222c5f8abbc5200f32e
Robin Malfait <malfait.robin@gmail.com>
2019-12-04 12:02:03 +0100

    add range, intersect and manhatten utils

--- src/utils.js
+++ src/utils.js
@@ -27,3 +27,33 @@ module.exports.abort = function abort() {
 module.exports.aborted = function aborted(err) {
   return err instanceof AbortError;
 };
+
+module.exports.range = function range(n, offset = 0) {
+  return Array.from({ length: n }, (v, k) => k + offset);
+};
+
+function intersectBetween(a, b) {
+  const [left, right] = [a, b].sort((a, b) => a.length - b.length);
+
+  return left.filter(element => right.includes(element));
+}
+
+module.exports.intersect = function intersect(...args) {
+  if (args.length <= 0) {
+    return [];
+  }
+
+  if (args.length === 1) {
+    return args[0];
+  }
+
+  if (args.length === 2) {
+    return intersectBetween(args[0], args[1]);
+  }
+
+  return args.reduce((a, b) => intersectBetween(a, b));
+};
+
+module.exports.manhatten = function manhatten([x0, y0], [x1, y1]) {
+  return Math.abs(x1 - x0) + Math.abs(y1 - y0);
+};
