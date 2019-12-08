aaab433a214eab2927a5e9fba984495535b22a43
Robin Malfait <malfait.robin@gmail.com>
2019-12-08 17:38:27 +0100

    add bonus day 8 - added support for borders & scaling the output

--- src/day-8/index.test.js
+++ src/day-8/index.test.js
@@ -1,7 +1,7 @@
 const { read } = require("../utils");
 
 const part1 = require("./part-1");
-const part2 = require("./part-2");
+const { process, flatten, render, scale, border, raw } = require("./part-2");
 
 describe("Part 1", () => {
   it.each([["123456789012", 3, 2, 1]])(
@@ -22,8 +22,12 @@ describe("Part 1", () => {
 describe("Part 2", () => {
   it.each([["0222112222120000", 2, 2, "0110"]])(
     'An input of %s with width=%s and height=%s results in a value of "%s"',
-    (image, width, height, output) => {
-      expect(part2.decode(image, width, height)).toEqual(output);
+    async (image, width, height, output) => {
+      expect(
+        await process(image, width, height)
+          .then(flatten())
+          .then(raw())
+      ).toEqual(output);
     }
   );
 
@@ -31,6 +35,12 @@ describe("Part 2", () => {
   it("should produce the correct value for the input data", async () => {
     const data = await read(__dirname, "./data.txt");
 
-    expect(part2.render(data, 25, 6)).toMatchSnapshot();
+    expect(
+      await process(data, 25, 6)
+        .then(flatten())
+        .then(border(2))
+        .then(scale(2, 1))
+        .then(render())
+    ).toMatchSnapshot();
   });
 });
--- src/day-8/part-2.js
+++ src/day-8/part-2.js
@@ -1,25 +1,33 @@
 // Day 8: Space Image Format
 
-const { chunk } = require("../utils");
+const { chunk, range } = require("../utils");
 
 const BLACK = "0";
 const WHITE = "1";
 const TRANSPARENT = "2";
 
-module.exports.decode = function(data, width, height) {
-  const layers = chunk(data, width * height);
+function process(data, width, height) {
+  return Promise.resolve({ data, width, height });
+}
 
-  return layers.reduce((final_layer, layer) => {
-    if (!final_layer.includes(TRANSPARENT)) {
-      return final_layer;
-    }
+function flatten() {
+  return ({ data, width, height }) => {
+    const layers = chunk(data, width * height);
 
-    return final_layer
-      .split("")
-      .map((value, index) => (value === TRANSPARENT ? layer[index] : value))
-      .join("");
-  }, TRANSPARENT.repeat(width * height));
-};
+    const flattened = layers.reduce((final_layer, layer) => {
+      if (!final_layer.includes(TRANSPARENT)) {
+        return final_layer;
+      }
+
+      return final_layer
+        .split("")
+        .map((value, index) => (value === TRANSPARENT ? layer[index] : value))
+        .join("");
+    }, TRANSPARENT.repeat(width * height));
+
+    return { data: flattened, width, height };
+  };
+}
 
 const PIXELS = {
   [BLACK]: "▒",
@@ -27,9 +35,75 @@ const PIXELS = {
   [TRANSPARENT]: "░"
 };
 
-module.exports.render = function(data, width, height) {
-  const decoded_image = module.exports.decode(data, width, height);
-  const image_with_pixels = decoded_image.split("").map(value => PIXELS[value]);
-  const image = chunk(image_with_pixels.join(""), width).join("\n");
-  return `\n${image}\n`;
+function render(lookup = PIXELS) {
+  return ({ data, width }) => {
+    const image_with_pixels = data.split("").map(value => lookup[value]);
+    const image = chunk(image_with_pixels.join(""), width).join("\n");
+    return `\n${image}\n`;
+  };
+}
+
+function scale(factorX = 1, factorY = factorX) {
+  return ({ data, width, height }) => {
+    const next_width = width * factorX;
+    const next_height = height * factorY;
+
+    const canvas = chunk(data, width).map(row => {
+      // Scale the x-axis
+      const next_row = row
+        .split("")
+        .map(value => value.repeat(factorX))
+        .join("");
+
+      // Scale the y-axis
+      return range(factorY)
+        .map(() => next_row)
+        .join("");
+    });
+
+    return {
+      data: canvas.join(""),
+      width: next_width,
+      height: next_height
+    };
+  };
+}
+
+function border(top = 0, right = top, bottom = top, left = right) {
+  return ({ data, width, height }) => {
+    const BORDER_CHARACTER = BLACK;
+    const next_width = width + left + right;
+    const next_height = height + top + bottom;
+
+    const top_rows = range(top).map(() => BORDER_CHARACTER.repeat(next_width));
+    const center = chunk(data, width).map(row => {
+      return [
+        BORDER_CHARACTER.repeat(left),
+        row,
+        BORDER_CHARACTER.repeat(right)
+      ].join("");
+    });
+    const bottom_rows = range(bottom).map(() =>
+      BORDER_CHARACTER.repeat(next_width)
+    );
+
+    return {
+      data: [...top_rows, ...center, ...bottom_rows].join(""),
+      width: next_width,
+      height: next_height
+    };
+  };
+}
+
+function raw() {
+  return ({ data }) => data;
+}
+
+module.exports = {
+  process,
+  flatten,
+  render,
+  scale,
+  border,
+  raw
 };
